ea89266005f8dd223649d41cb3d0230d7e41a02a
AlexanderPortland <alexanderportland1@gmail.com>
2025-10-23 16:24:56 -0400

    rework annotation logic to use macros

--- crates/sniff-test-attrs/src/lib.rs
+++ crates/sniff-test-attrs/src/lib.rs
@@ -3,30 +3,25 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
-const TOOL_ENV_VAR: &str = "USING_SNIFF_TOOL";
+macro_rules! define_sniff_tool_annotation {
+    ($name: ident) => {
+        #[proc_macro_attribute]
+        pub fn $name(_attr: TokenStream, item: TokenStream) -> TokenStream {
+            let mut t = TokenStream::new();
 
-#[proc_macro]
-pub fn tool_env_var(_item: TokenStream) -> TokenStream {
-    format!("\"{TOOL_ENV_VAR}\"",).parse().unwrap()
-}
+            // If we're registering the sniff-test tool, add the actual attribute to check unsafe.
+            if let Ok(rust_flags) = std::env::var("RUSTFLAGS")
+                && rust_flags.contains("-Zcrate-attr=register_tool(sniff_tool)")
+            {
+                t.extend(TokenStream::from(quote!(
+                    #[sniff_tool::$name]
+                )));
+            }
 
-#[proc_macro_attribute]
-pub fn check_unsafe(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut t = TokenStream::new();
-
-    // println!("expanding check unsafe!!");
-    // let env_vars = ;
-
-    // println!("vars are {env_vars:?}");
-
-    if let Ok(rust_flags) = std::env::var("RUSTFLAGS") {
-        if rust_flags.contains("-Zcrate-attr=register_tool(sniff_tool)") {
-            t.extend(TokenStream::from(quote!(
-                #[sniff_tool::check_unsafe]
-            )));
+            t.extend(item);
+            t
         }
-    }
-
-    t.extend(item);
-    t
+    };
 }
+
+define_sniff_tool_annotation!(check_unsafe);
