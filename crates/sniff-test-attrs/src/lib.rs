2b4a81d492f63e6e90c09db35dd12688d5070e5d
AlexanderPortland <alexanderportland1@gmail.com>
2025-10-23 15:05:57 -0400

    fix sniff-test macro expansion when building normally

--- crates/sniff-test-attrs/src/lib.rs
+++ crates/sniff-test-attrs/src/lib.rs
@@ -3,13 +3,30 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
+const TOOL_ENV_VAR: &str = "USING_SNIFF_TOOL";
+
+#[proc_macro]
+pub fn tool_env_var(_item: TokenStream) -> TokenStream {
+    format!("\"{TOOL_ENV_VAR}\"",).parse().unwrap()
+}
+
 #[proc_macro_attribute]
 pub fn check_unsafe(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attrs = quote!(
-        #[sniff_tool::check_unsafe]
-    );
     let mut t = TokenStream::new();
-    t.extend(TokenStream::from(attrs));
+
+    // println!("expanding check unsafe!!");
+    // let env_vars = ;
+
+    // println!("vars are {env_vars:?}");
+
+    if let Ok(rust_flags) = std::env::var("RUSTFLAGS") {
+        if rust_flags.contains("-Zcrate-attr=register_tool(sniff_tool)") {
+            t.extend(TokenStream::from(quote!(
+                #[sniff_tool::check_unsafe]
+            )));
+        }
+    }
+
     t.extend(item);
     t
 }
