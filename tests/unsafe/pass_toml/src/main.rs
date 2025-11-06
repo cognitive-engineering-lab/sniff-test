801384b80dac856307ddf515f4c51ac85829cb7a
Yash Agrawal <yagrawal2023@gmail.com>
2025-11-06 17:37:13 -0500

    added basic test for toml file

---
+++ tests/unsafe/pass_toml/src/main.rs
@@ -0,0 +1,15 @@
+unsafe fn foo(ptr: *const i32) -> i32 {
+    unsafe { *ptr }
+}
+
+#[sniff_test_attrs::check_unsafe]
+fn main() {
+    let x = 1;
+
+    unsafe {
+        /// Safety:
+        /// * non-null: a pointer that comes from a reference is trivially non-null
+        /// * aligned: a pointer that comes from a reference is trivially aligned
+        foo(&raw const x);
+    }
+}
