f97435c4def903f8c76dcc32a504c700b9233aa0
AlexanderPortland <alexanderportland1@gmail.com>
2025-11-24 12:51:09 -0500

    add tests for doc comment detection

---
+++ tests/unsafe/placement/call_direct.rs
@@ -0,0 +1,18 @@
+#![allow(unused_doc_comments)]
+extern crate sniff_test_attrs;
+
+#[sniff_test_attrs::check_unsafe]
+fn main() {
+    let x = 10;
+    let ptr = &raw const x;
+    unsafe {
+        /// SAFETY: ...
+        deref(ptr);
+    }
+}
+
+/// # Safety
+/// `ptr` must be aligned, non-null, etc.
+unsafe fn deref(ptr: *const i32) -> i32 {
+    unsafe { *ptr }
+}
